9dadd861472f039441fbbbd8937c5d97fe1f9c95
pk <pradeepkharb@hotmail.com>
2018-12-15 23:01:43 -0800

    connection successful with MLab database

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ require('dotenv').config();
 const
     express = require('express'),
     app = express(),
-    PORT = 3000;
+    PORT = 3000,
+    mongoose = require('mongoose'),
+    mongooseConnectionString = 'mongodb://admin:assembly1234@ds135704.mlab.com:35704/project_1';
 
+// mongoose connection
+mongoose.connect(mongooseConnectionString, err => {
+    console.log(err || `Connected to MLab (Project_1)`);
+});
 
 
 // listen to port
