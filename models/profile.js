959036faa41005d15ccb3f370f79da3329a8f05c
pk <pradeepkharb@hotmail.com>
2018-12-16 13:47:33 -0800

    created profileSchema and created a Profile model using profileSchema

---
+++ models/profile.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+}, { timestamps:true });
+
+const Profile = mongoose.model('Profile', profileSchema);
+module.exports = Profile;
