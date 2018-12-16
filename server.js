6563d95237f6fabfcf0fb4ef7ad8a9f363fd9fd5
pk <pradeepkharb@hotmail.com>
2018-12-15 22:54:36 -0800

    connected to localhost:3000

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+require('dotenv').config();
+
+const
+    express = require('express'),
+    app = express(),
+    PORT = 3000;
+
+
+
+// listen to port
+app.listen(PORT, err => {
+    console.log(err || `Connected to PORT: ${PORT}`);
+});
+
+
+
 
