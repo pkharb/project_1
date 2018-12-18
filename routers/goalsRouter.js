add80c0057dbe8104fd7e181449018132266af03
pk <pradeepkharb@hotmail.com>
2018-12-18 10:34:22 -0800

    added goals page and goalsRouter

--- routers/goalsRouter.js
+++ routers/goalsRouter.js
@@ -1,13 +1,42 @@
 const 
     express = require('express'),
-    Goal = require('../controllers/goals'),
-    router = express.Router();
+    Goal = require('../models/goals'),
+    goalsRouter = new express.Router();
 
 // routers
-router.post('/', Goal.create);
-router.get('/', Goal.index);
-router.get('/:id', Goal.show);
-router.patch('/:id', Goal.update);
-router.delete('/:id', Goal.destroy);
+goalsRouter.post('/', (req, res) => {
+    Goal.create(req.body, (err, newGoal) => {
+        if (err) res.json({ success: false, err});
+                 res.json({ success: true, newGoal});
+    })
+});
+goalsRouter.get('/', (req, res) => {
+    Goal.find({}, (err, goals) => {
+        if (err) res.json({ success: false, err});
+        res.json({ success: true, goals});
+    })
+});
+goalsRouter.get('/:id', (req, res) => {
+    Goal.findById(req.params.id, (err, goal) => {
+        if (err) res.json({ success: false, err});
+        res.json({ success: true, goal});
+    })
+});
+goalsRouter.patch('/:id', (req, res) => {
+    Goal.findByIdAndUpdate(req.params.id, req.body, (err, updatedGoal) => {
+        if (err) res.json({ success: false, err});
+        res.json({ success: true, updatedGoal});
+    })
+});
+goalsRouter.delete('/:id', (req, res) => {
+    Goal.findByIdAndDelete(req.params.id, (err, deletedGoal) => {
+        if (err) res.json({ success: false, err});
+        res.json({ success: true, deletedGoal});
+    })
+});
 
-module.exports = router;
+module.exports = goalsRouter;
+
+
+
+
